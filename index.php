e96fdbf06119262b63e0a188b8d86d9967c8efd9
mhd jeffry <mhdjeffryfadhil@gmail.com>
2025-07-02 15:03:33 +0700

    title web

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
     <link rel="icon" href="favicon.ico" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Login Perpustakaan</title>
+  <title>Login Perpustakaan Aku</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
